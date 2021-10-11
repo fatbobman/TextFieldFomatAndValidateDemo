4b83a43e2c13d0f1af695cc1ba209ffa0a7f0fb9
Yang Xu <xuyang@me.com>
2021-10-11 09:09:47 +0800

    Initial Commit

---
+++ TextFieldFomatAndValidate/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TextFieldFomatAndValidate
+//
+//  Created by Yang Xu on 2021/10/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TextFieldFomatAndValidate/TextFieldFomatAndValidateApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextFieldFomatAndValidateApp.swift
+//  TextFieldFomatAndValidate
+//
+//  Created by Yang Xu on 2021/10/11.
+//
+
+import SwiftUI
+
+@main
+struct TextFieldFomatAndValidateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
